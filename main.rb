c0e9e3f590a1b293a668f64eee5425412670bf7c
billyvollman <b.vollman@gmail.com>
2019-08-01 09:29:35 +1000

    setup create account page and linked it to user database

--- main.rb
+++ main.rb
@@ -11,7 +11,24 @@ get '/' do
   erb :index
 end
 
+get '/createaccount' do
+  erb :createaccount
+end
 
+post '/users' do
+  user = User.new
+  user.username = params[:username]
+  user.firstname = params[:firstname]
+  user.lastname = params[:lastname]
+  user.street_address = params[:street_address]
+  user.city = params[:city]
+  user.postcode = params[:postcode]
+  user.country = params[:country]
+  user.email = params[:email]
+  user.password = params[:password]
+  user.save
+  erb :index
+end
 
 
 
