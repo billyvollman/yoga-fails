f5c6f5b502b0fb1da048683d7829c3540cb83990
billyvollman <b.vollman@gmail.com>
2019-08-02 10:13:23 +1000

    add show button for password input on create account page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,14 @@
 var showButton = document.querySelector('.showButton')
 var password = document.querySelector('.passwordInput')
 
-console.log("testing")
-
 var changeInput = function() {
     if (showButton.textContent === "show") {
         password.type = "text"
         showButton.textContent = "hide"
-        console.log('working') }
+        }
     else {
         showButton.textContent = "show"
         password.type = "password"
-        console.log('working two') 
     }
 }
 
