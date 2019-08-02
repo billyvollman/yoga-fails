f3807b2ba1e7dbd5b0422afa499d7eb79b60e099
billyvollman <b.vollman@gmail.com>
2019-08-02 17:44:34 +1000

    changed homepage formatting of images

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-# require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 require_relative 'database_config'
 require_relative 'models/fail'
 require_relative 'models/comment'
