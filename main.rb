eb7810294f608a5775ac222c81a6cdee8e1e6d93
billyvollman <b.vollman@gmail.com>
2019-09-30 23:33:57 +1000

    removed reloaders from main rb to see if it helps to stop app from crashing on heroku

--- main.rb
+++ main.rb
@@ -1,11 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require_relative 'database_config'
-
-also_reload File.expand_path(__dir__, 'models/*')
-also_reload File.expand_path(__dir__, 'views/*')
-also_reload File.expand_path(__dir__, 'routes/*')
-
 require_relative 'models/fail'
 require_relative 'models/comment'
 require_relative 'models/user'
